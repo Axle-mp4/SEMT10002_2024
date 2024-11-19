1b2b164617565ff4bcef67d21b6ee9c2f924514e
Alex D <axled1005@outlook.com>
2024-11-19 13:09:06 +0000

    commit message good enough m8

---
+++ consolidation_exercises/Week 7 exercises.py
@@ -0,0 +1,66 @@
+#    student_answers = [[6, 1, 4], [3, 8, 4], [7, 4, 5], [4, 7, 3]]
+#    average_score = []
+#
+#    for i in range(len(student_answers[0])):
+#        a = student_answers[0][i]+student_answers[1][i]+student_answers[2][i]+student_answers[3][i]
+#        b = a/4
+#        average_score.append(b)
+#    print(average_score)
+
+
+
+
+
+
+
+#list_of_numbers = [1, 3, 6, 10]
+
+#cumulative_sum = []
+#Sum = 0
+
+#for ii in range(len(list_of_numbers)):
+#    Sum = Sum + list_of_numbers[ii]
+#    cumulative_sum.append(Sum)
+#print(cumulative_sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#square_numbers = []
+#
+#for i in range(50):
+#    s = i*i
+#    print(s)
+#    if s < 1000:
+#        square_numbers.append(s)
+#
+#print(square_numbers)
+
+        
+
+#triangle_numbers = []
+
+#for n in range(200):
+#    if n*(n+1)*0.5 >= 1000:
+#        break
+#    else:
+#        T = (n*(n+1))*0.5
+#        triangle_numbers.append(T)
+#print(triangle_numbers)
+
+
+
+
+
+my_dict = {1: "bullshit"}
+print(my_dict[1])
